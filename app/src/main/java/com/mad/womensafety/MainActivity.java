332596a118be96279421ebfc6554d875ba3c9e80
parikshith shetty <ab007shetty@gmail.com>
2022-06-01 19:25:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/mad/womensafety/MainActivity.java
@@ -0,0 +1,194 @@
+package com.mad.womensafety;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mad.womensafety.R;
+import com.mad.womensafety.Contacts.ContactModel;
+import com.mad.womensafety.Contacts.CustomAdapter;
+import com.mad.womensafety.Contacts.DbHelper;
+import com.mad.womensafety.ShakeServices.ReactivateService;
+import com.mad.womensafety.ShakeServices.SensorService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int IGNORE_BATTERY_OPTIMIZATION_REQUEST = 1002;
+    private static final int PICK_CONTACT = 1;
+
+    //create instances of various classes to be used
+    Button button1;
+    ListView listView;
+    DbHelper db;
+    List<ContactModel> list;
+    CustomAdapter customAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission (MainActivity.this, Manifest.permission.SEND_SMS);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission (MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission (MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions (MainActivity.this, new String[]{Manifest.permission.SEND_SMS, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.READ_CONTACTS}, 0);
+        }
+
+        //this is a special permission required only by devices using
+        //Android Q and above. The Access Background Permission is responsible
+        //for populating the dialog with "ALLOW ALL THE TIME" option
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, 100);
+        }
+
+        FloatingActionButton floatingActionButton=findViewById (R.id.smsInst);
+        floatingActionButton.setOnClickListener (v -> startActivity( new Intent ( getApplicationContext(), SmsBtnInst.class ) ));
+
+        //check for BatteryOptimization,
+        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (pm != null && !pm.isIgnoringBatteryOptimizations(getPackageName())) {
+                askIgnoreOptimization();
+            }
+        }
+
+        //start the service
+        SensorService sensorService = new SensorService();
+        Intent intent = new Intent(this, sensorService.getClass());
+        if (!isMyServiceRunning(sensorService.getClass())) {
+            startService(intent);
+        }
+
+
+        button1 = findViewById(R.id.Button1);
+        listView=(ListView)findViewById(R.id.ListView);
+        db=new DbHelper(this);
+        list=db.getAllContacts();
+        customAdapter=new CustomAdapter(this,list);
+        listView.setAdapter(customAdapter);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calling of getContacts()
+                if(db.count()!=5) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                    startActivityForResult(intent, PICK_CONTACT);
+                }else{
+                    Toast.makeText(MainActivity.this, "Can't Add more than 5 Contacts", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    //method to check if the service is running
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                Log.i ("Service status", "Running");
+                return true;
+            }
+        }
+        Log.i ("Service status", "Not running");
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction("restartservice");
+        broadcastIntent.setClass(this, ReactivateService.class);
+        this.sendBroadcast(broadcastIntent);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==100){
+            if(grantResults[0]==PackageManager.PERMISSION_DENIED){
+                Toast.makeText(this, "Permissions Denied!\n Can't use the App!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //get the contact from the PhoneBook of device
+        switch (requestCode) {
+            case (PICK_CONTACT):
+                if (resultCode == Activity.RESULT_OK) {
+
+                    Uri contactData = data.getData();
+                    Cursor c = managedQuery(contactData, null, null, null, null);
+                    if (c.moveToFirst()) {
+
+                        String id = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+                        String hasPhone = c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                        String phone = null;
+                        try {
+                            if (hasPhone.equalsIgnoreCase("1")) {
+                                Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id,null, null);
+                                phones.moveToFirst();
+                                phone = phones.getString(phones.getColumnIndex("data1"));
+                            }
+                            String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                            db.addcontact(new ContactModel(0,name,phone));
+                            list=db.getAllContacts();
+                            customAdapter.refresh(list);
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    //this method prompts the user to remove any battery optimisation constraints from the App
+    private void askIgnoreOptimization() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            @SuppressLint("BatteryLife") Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, IGNORE_BATTERY_OPTIMIZATION_REQUEST);
+        }
+
+    }
+
+}
